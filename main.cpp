9c90f51eee7f235371d041c60c9b7ef6b6de0e5a
samarche92 <samar.chehade.sc@gmail.com>
2020-01-31 13:04:43 -0500

    added Eigen

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include "functions.h"
+
+#include "Eigen/Dense"
 
 #include <time.h>
 #include <iostream>
@@ -19,10 +22,11 @@ using namespace std;
 
 using namespace Eigen;
 
-typedef Matrix<std::complex<double>, 3, 3> Matrix3c;
+typedef Matrix<std::complex<double>, 3, 3> MatriX3c;
 typedef Matrix<std::complex<double>, 3, 1> Vectorx3c;
 typedef Matrix<std::complex<double>,Dynamic,Dynamic> MatrixXdc;
 typedef Matrix<std::complex<double>, Dynamic, 1> VectorXdc;
+
 
 int main()
 {
