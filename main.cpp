dad4afa86dd9ce7a0e38771c8c8c4eb3f60c8cc0
samarche92 <samar.chehade.sc@gmail.com>
2020-01-31 12:44:36 -0500

    adding first files

---
+++ main.cpp
@@ -0,0 +1,31 @@
+
+#include <time.h>
+#include <iostream>
+#include <ctime>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <cstdlib>
+#include <memory>
+#include <thread>
+#include <string>
+#include <math.h>
+#include <cmath>
+#include <complex>
+
+#define Pi 3.14159265358979323846
+
+using namespace std;
+
+using namespace Eigen;
+
+typedef Matrix<std::complex<double>, 3, 3> Matrix3c;
+typedef Matrix<std::complex<double>, 3, 1> Vectorx3c;
+typedef Matrix<std::complex<double>,Dynamic,Dynamic> MatrixXdc;
+typedef Matrix<std::complex<double>, Dynamic, 1> VectorXdc;
+
+int main()
+{
+    cout<<"hello hello"<<endl;
+    return 0;
+}
