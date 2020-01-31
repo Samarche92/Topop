075b25dc59c0c291aebf41c63b8f406153a4e1a7
samarche92 <samar.chehade.sc@gmail.com>
2020-01-31 16:26:07 -0500

    cleaned up initialization + using arrays instead of matrices

--- main.cpp
+++ main.cpp
@@ -1,20 +1,5 @@
 #include "functions.h"
 
-#include "Eigen/Dense"
-
-#include <time.h>
-#include <iostream>
-#include <ctime>
-#include <stdio.h>
-#include <stdlib.h>
-#include <fstream>
-#include <cstdlib>
-#include <memory>
-#include <thread>
-#include <string>
-#include <math.h>
-#include <cmath>
-#include <complex>
 
 #define Pi 3.14159265358979323846
 
@@ -22,14 +7,36 @@ using namespace std;
 
 using namespace Eigen;
 
-typedef Matrix<std::complex<double>, 3, 3> MatriX3c;
+/*typedef Matrix<std::complex<double>, 3, 3> MatriX3c;
 typedef Matrix<std::complex<double>, 3, 1> Vectorx3c;
 typedef Matrix<std::complex<double>,Dynamic,Dynamic> MatrixXdc;
-typedef Matrix<std::complex<double>, Dynamic, 1> VectorXdc;
+typedef Matrix<std::complex<double>, Dynamic, 1> VectorXdc;*/
 
 
 int main()
 {
     cout<<"hello hello"<<endl;
+
+    functions* MyFunc = new functions();
+
+    ///Initializing parameters
+    MyFunc->intialize();
+
+    int nelx=MyFunc->getnelx(),nely=MyFunc->getnely();
+    double volfrac=MyFunc->getvolfrac();
+
+    MatrixXd xold,x=MatrixXd::Constant(nely,nelx,volfrac);
+    int loop=0;
+    double change=1.0,c=0.0;
+
+    while (change>0.01)
+    {
+        loop++;
+        xold=x;
+
+        change=(x-xold).maxCoeff();
+        cout<<"It.: "<<loop<<" Obj.: "<<c<<" Vol.: "<<x.mean()<<" ch.: "<<change<<endl;
+    }
+
     return 0;
 }
